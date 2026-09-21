603835fdf2879840d2d7737db4a01f02cfc2ad8a
blesstonjeffrin <blesston.jeff@gmail.com>
2026-09-21 21:42:49 +0530

    Fix coupon product service URL configuration

--- coupon-restapp/src/main/java/com/wipro/coupon/client/ProductClient.java
+++ coupon-restapp/src/main/java/com/wipro/coupon/client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "product-restapp", url = "${PRODUCT_SERVICE_URL}")
+@FeignClient(name = "product-restapp", url = "${product.service.url}")
 public interface ProductClient {
 
     @PutMapping("/api/v1/products/{id}/discount")
